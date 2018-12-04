c73a390d1b9f6322dc7fbd4cad1b9eab050f7cc2
Mohamed Trabelsi <32022631+medtray@users.noreply.github.com>
2018-12-03 23:40:18 -0500

    Update add_country.java

--- final_project/src/add_country.java
+++ final_project/src/add_country.java
@@ -35,7 +35,7 @@ public class add_country extends HttpServlet {
  
         try {
         	
-            ConnectionPool pool = ConnectionPool.getInstance("m0trab01");
+            ConnectionPool pool = ConnectionPool.getInstance("database");
             con = pool.getConnection();
             
 
